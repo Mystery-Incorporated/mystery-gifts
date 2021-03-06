1635606bea37f86d00b6532356533f43a5a6213f
Kalindu De Costa <kalinduk.decosta@gmail.com>
2021-03-06 01:22:33 -0500

    helmet test

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: true} ));
 
-app.use(helmet());
+// app.use(helmet());
 // app.use(
 //     helmet({
 //       contentSecurityPolicy: false,
